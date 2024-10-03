d9302544e1e681c46616852bf7ac7588364d6f20
devansh9012 <tdevansh099@gmail.com>
2024-10-03 14:24:32 +0530

    sss

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -10,7 +10,7 @@ class StudentController extends Controller
 {
     public function syllabus(){
         try {
-            $data = SubCategory::where('status',1)->where('type','banner')->get();
+            $data = SubCategory::where('status',1)->where('sections','!=',null)->get();
             return response()->json(['status'=> true,'data' => $data], 201);
         } catch (\Throwable $th) {
             return response()->json(['status'=> false,'error' => $th->getMessage()], 500);
