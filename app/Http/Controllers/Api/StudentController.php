7d5e3ebe72d9ef02e21a8c7c85162a3f1fea3719
devansh9012 <tdevansh099@gmail.com>
2024-10-04 17:48:26 +0530

    ss

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use App\Models\Quizze;
 use App\Models\QuizType;
+use App\Models\PracticeSet;
 
 class StudentController extends Controller
 {
