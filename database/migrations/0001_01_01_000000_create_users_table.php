93d7a27b6181a73c4217747945e77b2192bc7826
devansh9012 <tdevansh099@gmail.com>
2024-10-04 10:11:59 +0530

    dsf

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
+            $table->string('image')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
