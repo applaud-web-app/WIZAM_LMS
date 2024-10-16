524d8bc996c5df356e4b4e47c4bbe94809cb36b9
devansh9012 <tdevansh099@gmail.com>
2024-10-16 15:00:45 +0530

    ddd

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -19,6 +19,6 @@ class Payment extends Model
     ];
 
     public function user(){
-        return $this->hasOne(User::class, 'user_id', 'id');
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -23,7 +23,7 @@ class Subscription extends Model
 
     
     public function user(){
-        return $this->hasOne(User::class, 'user_id', 'id');
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 
     public function plans()
