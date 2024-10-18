f5473519a0563fac3e9b25ae0ac6a29b6280d016
devansh9012 <tdevansh099@gmail.com>
2024-10-18 11:28:15 +0530

    sss

--- resources/views/setting/homepage-setting.blade.php
+++ resources/views/setting/homepage-setting.blade.php
@@ -242,53 +242,6 @@
                 <div
                     class="bg-white dark:bg-box-dark m-0 p-0 text-body dark:text-subtitle-dark text-[15px] rounded-10 relative">
                     <div class="p-[25px]">
-                        {{-- <form action="{{ route('update-help') }}" method="POST" autocomplete="off" enctype="multipart/form-data" id="addSetting">
-                            @csrf
-                            <h1 class="mb-4 text-xl"><b>Help Section</b></h1>
-                        
-                            <div class="mb-[15px]">
-                                <label for="title" class="inline-flex items-center w-[178px] mb-[10px] text-sm font-medium capitalize text-body dark:text-title-dark">
-                                    Title <span class="text-red-500">*</span>
-                                </label>
-                                <div class="flex flex-col flex-1">
-                                    <input type="text" id="title" name="title" class="rounded-4 border-normal border-1 text-[15px] dark:bg-box-dark-up dark:border-box-dark-up px-[20px] py-[12px] min-h-[50px] outline-none placeholder:text-[#A0A0A0] text-body dark:text-subtitle-dark w-full focus:ring-primary focus:border-primary" placeholder="Your Site Name" value="{{ old('title', $help->title ?? '') }}" required>
-                                </div>
-                            </div>
-                        
-                            @for ($i = 0; $i < 4; $i++)
-                                <div class="border-b-2 mb-[15px]">
-                                    <h2 class="slide-title text-lg mb-3"><b>Card {{ $i + 1 }}</b></h2>
-                                    <div class="mb-[15px]">
-                                        <label for="card_title[]" class="inline-flex items-center w-[178px] mb-[10px] text-sm font-medium capitalize text-body dark:text-title-dark">
-                                            Card Image <span class="text-red-500">*</span>
-                                        </label>
-                                        <div class="flex flex-col flex-1">
-                                            <input type="file" name="card_image[]" class="relative m-0 block w-full min-w-0 flex-auto cursor-pointer rounded border border-solid  bg-transparent bg-clip-padding px-3 py-[0.32rem]  font-normal text-surface transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:me-3 file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-e file:border-solid file:border-inherit file:bg-transparent file:px-3  file:py-[0.32rem] file:text-surface focus:border-primary focus:text-gray-700 focus:shadow-inset focus:outline-none dark:border-white/70 dark:text-white  file:dark:text-white" required>
-                                        </div>
-                                    </div>
-                                    <div class="mb-[15px]">
-                                        <label for="card_title[]" class="inline-flex items-center w-[178px] mb-[10px] text-sm font-medium capitalize text-body dark:text-title-dark">
-                                            Card Title <span class="text-red-500">*</span>
-                                        </label>
-                                        <div class="flex flex-col flex-1">
-                                            <input type="text" name="card_title[]" class="rounded-4 border-normal border-1 text-[15px] dark:bg-box-dark-up dark:border-box-dark-up px-[20px] py-[12px] min-h-[50px] outline-none placeholder:text-[#A0A0A0] text-body dark:text-subtitle-dark w-full focus:ring-primary focus:border-primary" placeholder="Your Card Title" value="{{ old('card_title.' . $i, optional(json_decode($help->extra ?? null))[$i]->title ?? '') }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="mb-[15px]">
-                                        <label for="card_description[]" class="inline-flex items-center w-[178px] mb-[10px] text-sm font-medium capitalize text-body dark:text-title-dark">
-                                            Card Description <span class="text-red-500">*</span>
-                                        </label>
-                                        <div class="flex flex-col flex-1">
-                                            <textarea name="card_description[]" class="rounded-4 border-normal border-1 text-[15px] dark:bg-box-dark-up dark:border-box-dark-up px-[20px] py-[12px] min-h-[50px] outline-none placeholder:text-[#A0A0A0] text-body dark:text-subtitle-dark w-full focus:ring-primary focus:border-primary" maxlength="300" placeholder="Your Card Description" required>{{ old('card_description.' . $i, optional(json_decode($help->extra ?? null))[$i]->description ?? '') }}</textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endfor
-                        
-                            <div class="mb-[15px]">
-                                <button type="submit" class="mt-3 bg-primary text-white py-[12px] px-[20px] rounded-4 border-none cursor-pointer hover:bg-primary-dark focus:ring-primary focus:border-primary">Submit</button>
-                            </div>
-                        </form> --}}
                         <form action="{{ route('update-help') }}" method="POST" autocomplete="off" enctype="multipart/form-data" id="addSetting">
                             @csrf
                             <h1 class="mb-4 text-xl"><b>Help Section</b></h1>
@@ -350,13 +303,15 @@
                                         <div class="flex flex-wrap items-center gap-[10px]">
                                             <!--First radio-->
                                             <div class="mb-[0.125rem] mr-4 inline-block min-h-[1.5rem] pl-[1.5rem]">
-                                            <input class="relative ltr:float-left rtl:float-right -ms-[1.5rem] me-1 mt-0.5 h-[18px] w-[18px] appearance-none rounded-full border-1 border-solid border-normal before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary" type="radio" name="card_status[]" id="status" value="1" autocompleted="" {{ old('card_status.' . $i, optional(json_decode($help->extra ?? null))[$i]->status == 1 ? 'checked' : '' ?? '') }}>
-                                            <label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="statusRadio1">Enable</label>
+                                                <input class="relative ltr:float-left rtl:float-right -ms-[1.5rem] me-1 mt-0.5 h-[18px] w-[18px] appearance-none rounded-full border-1 border-solid border-normal before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary" 
+                                                type="radio" name="card_status[{{ $i }}]" value="1" {{ old('card_status.' . $i, optional(json_decode($help->extra ?? null))[$i]->status ?? '') == 1 ? 'checked' : '' }}>
+                                                <label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="statusRadio1">Enable</label>
                                             </div>
-                                            <!--Second radio-->
+                                            
                                             <div class="mb-[0.125rem] mr-4 inline-block min-h-[1.5rem] pl-[1.5rem]">
-                                            <input class="relative ltr:float-left rtl:float-right -ms-[1.5rem] me-1 mt-0.5 h-[18px] w-[18px] appearance-none rounded-full border-1 border-solid border-normal before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary" type="radio" name="card_status[]" id="status" value="0" autocompleted="" {{ old('card_status.' . $i, optional(json_decode($help->extra ?? null))[$i]->status == 0 ? 'checked' : '' ?? '') }}>
-                                            <label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="statusRadio2">Disable</label>
+                                                <input class="relative ltr:float-left rtl:float-right -ms-[1.5rem] me-1 mt-0.5 h-[18px] w-[18px] appearance-none rounded-full border-1 border-solid border-normal before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary" 
+                                                type="radio" name="card_status[{{ $i }}]" value="0" {{ old('card_status.' . $i, optional(json_decode($help->extra ?? null))[$i]->status ?? '') == 0 ? 'checked' : '' }}>
+                                                <label class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer" for="statusRadio2">Disable</label>
                                             </div>
                                         </div>
                                     </div>
