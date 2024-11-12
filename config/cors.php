2a49f0c9cf4dc876cbf82f7b3d48d7fc333e50d4
devansh9012 <tdevansh099@gmail.com>
2024-11-12 10:29:42 +0530

    dsfdsf

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://wizam-nextjs.vercel.app','http://localhost:3000'],
+    'allowed_origins' => ['https://wizam-nextjs.vercel.app','http://localhost:3000','https://wizam.uk'],
 
     'allowed_origins_patterns' => [],
 
