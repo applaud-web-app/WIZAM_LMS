60e0988eec53e50cb21ddfd95b1b3c26789c25ac
devansh9012 <tdevansh099@gmail.com>
2024-11-13 12:10:05 +0530

    sdfsdf

--- resources/views/manageTest/quizzes/quiz-report-detail.blade.php
+++ resources/views/manageTest/quizzes/quiz-report-detail.blade.php
@@ -47,7 +47,7 @@
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
                     <!-- Grid items -->
                     <div class="bg-white border-1 rounded-lg transition duration-300 ease-in-out hover:bg-green-500 dark:bg-gray-700 dark:hover:bg-green-600 py-[30px] px-[15px] text-center">
-                    <h3 class="mb-2">quiz STATUS</h3>
+                    <h3 class="mb-2">QUIZ STATUS</h3>
                     <p><span class=' capitalize font-medium inline-flex items-center justify-center min-h-[24px]  text-{{$statusColor}} '>{{(float)$quizResult->student_percentage >= (float)$quizResult->pass_percentage ? "PASS" : "FAIL"}}</span></p>
                     </div>
                     <div class="bg-white border-1 rounded-lg transition duration-300 ease-in-out hover:bg-green-500 dark:bg-gray-700 dark:hover:bg-green-600 py-[30px] px-[15px] text-center">
