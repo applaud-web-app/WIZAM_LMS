befe68711f71ef8932de43a8f7ab0ad709b78512
devansh9012 <tdevansh099@gmail.com>
2024-11-18 16:40:58 +0530

    gasdas

--- app/Http/Controllers/Api/CmsController.php
+++ app/Http/Controllers/Api/CmsController.php
@@ -267,7 +267,7 @@ class CmsController extends Controller
             ->leftJoin('questions', 'exam_questions.question_id', '=', 'questions.id') // Join with questions
             ->selectRaw('COUNT(questions.id) as questions_count') // Count of questions
             ->selectRaw('SUM(CAST(questions.default_marks AS DECIMAL)) as total_marks') // Sum of default_marks
-            ->where(['exams.favourite' => 1, 'exams.status' => 1,'exams.is_public' => 1])
+            ->where(['exams.status' => 1,'exams.is_public' => 1])
             // ->where('exam_schedules.status', 1)
             ->groupBy('exams.id', 'exams.img_url', 'exams.title', 'exams.description', 'exams.price', 'exams.is_free', 'exams.slug', 'exams.exam_duration','exams.exam_type_id','exams.subcategory_id',)
             // 'exam_schedules.schedule_type',
