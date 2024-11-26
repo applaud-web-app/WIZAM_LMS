8f0254e080c406c819957307706955989d3ded42
devansh9012 <tdevansh099@gmail.com>
2024-11-26 11:12:47 +0530

    sdffs

--- app/Http/Controllers/admin/PaymentController.php
+++ app/Http/Controllers/admin/PaymentController.php
@@ -871,11 +871,13 @@ class PaymentController extends Controller
                $endpointSecret
          );
 
+         \Log::error('Stripe webhook error: ' . $event->type);
+
          // Handle specific events
          switch ($event->type) {
                case 'checkout.session.completed':
                   $session = $event->data->object;
-
+                  \Log::error('Stripe webhook error: ' . $session);
                   // Handle completed checkout sessions
                   $this->handleCheckoutSessionCompleted($session);
                   break;
