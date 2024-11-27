9325991b1d3670d1ca8a7681f86cdb9c06a86a72
devansh9012 <tdevansh099@gmail.com>
2024-11-27 18:27:36 +0530

    dsfsf

--- app/Http/Controllers/Api/PracticeSetController.php
+++ app/Http/Controllers/Api/PracticeSetController.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Str;
 use Carbon\Carbon;
 use App\Models\Question;
 use App\Models\Subscription;
+use App\Models\SubscriptionItem;
+use App\Models\GroupUsers;
 use App\Models\Plan;
 use App\Models\User;
 
@@ -95,6 +97,9 @@ class PracticeSetController extends Controller
                 ->where('practice_sets.subcategory_id', $request->category)
                 ->where('practice_sets.status', 1)
                 ->where('questions.status', 1)
+                ->where(function ($query) use ($purchasePractice) {
+                    $query->WhereIn('practice_sets.id', $purchasePractice); 
+                })
                 ->groupBy('practice_sets.id', 'practice_sets.title', 'practice_sets.description', 'practice_sets.slug', 'practice_sets.subcategory_id', 'practice_sets.status', 'practice_sets.allow_reward', 'practice_sets.reward_popup', 'practice_sets.point_mode', 'practice_sets.points', 'practice_sets.is_free')
                 ->first();
 
